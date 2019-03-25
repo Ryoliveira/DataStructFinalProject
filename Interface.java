056f7301ed1e17740662ff6583c64c491e5437d3
SamieMenon <42633818+SamieMenon@users.noreply.github.com>
2019-03-25 16:55:55 -0700

    Add files via upload
    Final interface class

--- Interface.java
+++ Interface.java
@@ -1,19 +1,23 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Scanner;
 import Objects.*;
-import Objects.Heap;
 
 public class Interface {
 	private ArrayList<Employee> EmployeeDirectory = new ArrayList<Employee>();
-	private ArrayList<Customer> CustomerDirectory = new ArrayList<Customer>();
-//	private ArrayList<Shipment> ShipmentDirectory = new ArrayList<Shipment>();
-	SongsDatabase s = new SongsDatabase();
+	private HashTable<Customer> CustomerDirectory = new HashTable<Customer>(17);
+	SongsDatabase s= new SongsDatabase();
+	int orderNum=100;
 	Heap unShipped = new Heap(100);
 	Heap shipped = new Heap(100);
+	
+	public void orderNum() {
+		orderNum++;
+	}
 	
 	public void addSongs() {
 		try {
@@ -23,6 +27,7 @@ public class Interface {
 		}
 	}
 	
+	@SuppressWarnings("static-access")
 	public void searchSongs(Scanner sc) {
 		System.out.println("\nSearching for a song!\n");
 		boolean dummy = true;
@@ -76,7 +81,78 @@ public class Interface {
 		i.empMenu();
 		
 	}
+	/////////////////////////////////////////SHIPMENT
 	
+	/**
+	 * 
+	 */
+	public void currentOrder() throws IOException {
+		System.out.println("\n****CURRENT ORDERS****\n");
+		File orderInfile = new File("currentOrders.txt");
+		Scanner in = new Scanner(orderInfile);
+		while (in.hasNextLine()) {
+			String orderNum = in.nextLine();
+			int num = Integer.parseInt(orderNum);
+			String numItems = in.nextLine();
+			int numItem = Integer.parseInt(numItems);
+			String priority = in.nextLine();
+			int p;
+			if(priority.equalsIgnoreCase("overnight")) {
+				p = 1;
+			}
+			else if(priority.equalsIgnoreCase("rush")) {
+				p = 2;
+			}
+			else{
+				p = 3;
+			}
+			int year = in.nextInt();
+			int month = in.nextInt();
+			int day = in.nextInt();
+			in.nextLine();
+			Calendar cal = Calendar.getInstance();
+			cal.set(year, month, day);
+			String status = in.nextLine();
+			Order o = new Order(num, numItem, p, cal, status);
+			if(status.equalsIgnoreCase("shipped")) {
+				shipped.insert(o);
+			}
+			else {
+				unShipped.insert(o);
+			}
+		}
+//		printShippedOrders();
+//		printUnshippedOrders();
+	}
+	public void printShippedOrders() {
+		System.out.println("Shipped Orders: ");
+		shipped.displayArray();
+	}
+	
+	public void printUnshippedOrders() {
+		System.out.println("Unshipped Orders: ");
+		unShipped.displayArray();
+	}
+	
+	/**
+	 * 
+	 */
+	public void Shipment() throws IOException {
+		currentOrder();
+		System.out.println("\n****SHIPMENT DETAILS****\n");
+		System.out.print("These are the ");
+		printUnshippedOrders();
+		Order o = unShipped.getMax();
+		System.out.println("Shipping Order: \n" + o.toString());
+		unShipped.remove(1);
+		shipped.insert(o);
+		System.out.println("Order Number: " + o.getOrderNum() + " has been shipped!\n");
+		System.out.print("Remaining ");
+		printUnshippedOrders();
+	}
+	
+	
+	////////////////////////////////
 	
 	
 	/**
@@ -177,10 +253,10 @@ public class Interface {
 	 * @throws IOException 
 	 * 
 	 */
-	public void empMenu(){
+	public void empMenu() throws IOException {
 		Scanner sc = new Scanner(System.in);
-		int choice = 0;
-		while(choice!=7) {
+		int choice=0;
+		while (choice!=7) {
 			System.out.println("**Main Menu**\n\n" + 
 					"1. View Current Orders \n" + 
 					"2. View shipment(Ship an order)\n"+
@@ -191,78 +267,77 @@ public class Interface {
 					"7. Exit");
 			System.out.print("\nPlease enter your choice(1-7):");
 			choice = sc.nextInt();
-			if (choice == 1) {
-				try {
-					currentOrder();
-				} catch (IOException e) {
-					System.out.print("No current orders in processing");
-				}
-			} else if (choice == 2) {
-				try {
-					Shipment();
-				}
-				catch(Exception e) {
-					System.out.println(e.toString());
-				}
-			} else if (choice == 3) {
-				sc.nextLine();
-				displaySongs(sc);
-			} else if (choice == 4) {
-				for (int i=0; i<CustomerDirectory.size(); i++) {
-					System.out.println("\n");
-					System.out.println(CustomerDirectory.get(i));
-				}
-			} else if (choice == 5) {
-				System.out.print("Enter Customer's first name:");
-				String custName = sc.next();
-				boolean temp= false;
-				for (int i=0; i<CustomerDirectory.size(); i++) {
-					if (CustomerDirectory.get(i).getFirstName().equalsIgnoreCase(custName)) {
-						System.out.println("We found a customer with the entered name! \n\n");
-						System.out.println(CustomerDirectory.get(i));
-						temp= true;
-					}
-				}
-				if (temp==false) {
-					System.out.println("No matches found! ");
-				}
-			} 
-			else if (choice == 6) {
-				boolean loop = true;
-				while (loop) {
-
-					System.out.println("Please select from one of the following options:\n");
-					System.out.println("A. Add a song\n"
-							         + "D. Display songs\n"
-							         + "R. Remove a song\n"
-							         + "S. Search for a song\n"
-							         + "X. Exit\n");
-					System.out.print("Enter your choice: ");
-					String custName = sc.nextLine();
-					if (!custName.matches("(?i)A|D|R|X|S")) {
-						System.out.println("\nInvalid Selection!\n");
-					} else if (custName.matches("(?i)A")) {
-						System.out.println("\nAdding a song!\n");
-						s.addSong(sc);
-					} else if (custName.matches("(?i)D")) {
-						displaySongs(sc);
-					} else if (custName.matches("(?i)R")) {
-						System.out.println("\nRemoving a song!\n");
-						s.removeSong();
-					} else if (custName.matches("(?i)S")) {
-						searchSongs(sc);
-					} else if (custName.matches("(?i)X")) {
-						loop = false;
-						System.out.println("\nReturning to main menu...\n");
-					}
-				}
+		if (choice == 1) {
+			try {
+				currentOrder();
+			} catch (IOException e) {
+				System.out.print("No current orders in processing");
 			}
-
-			else if (choice == 7) {
-				System.out.println("\nGoodbye! ");
+			
+		} 		 
+		else if (choice == 2) {
+			Shipment();
+			
+		} 
+		else if (choice == 3) {
+			sc.nextLine();
+			displaySongs(sc);
+		} 
+		else if (choice == 4) {
+			for (int i=0; i<CustomerDirectory.getNumElements()-1; i++) {
+				System.out.println("\n");
+				CustomerDirectory.printBucket(i);
+			}
+			
+		} else if (choice == 5) {
+			System.out.print("Enter Customer's first name:");
+			String FirstName=sc.next();
+			System.out.print("Enter Customer's last name:");
+			String LastName=sc.next();
+			Customer tempCustomer = new Customer(FirstName, LastName);
+				if (CustomerDirectory.search(tempCustomer) != -1) {
+					System.out.println(tempCustomer.getFirstName() +" is in database \n");
+				}
+				else {
+				System.out.println("No matches found! ");
+				}
+			
+		} 
+		else if (choice == 6) {
+			boolean loop = true;
+			while (loop) {
+				System.out.println("Please select from one of the following options:\n");
+				System.out.println("A. Add a song\n"
+						         + "D. Display songs\n"
+						         + "R. Remove a song\n"
+						         + "S. Search for a song\n"
+						         + "X. Exit\n");
+				System.out.print("Enter your choice: ");
+				String custName = sc.nextLine();
+				if (!custName.matches("(?i)A|D|R|X|S")) {
+					System.out.println("\nInvalid Selection!\n");
+				} else if (custName.matches("(?i)A")) {
+					System.out.println("\nAdding a song!\n");
+					s.addSong(sc);
+				} else if (custName.matches("(?i)D")) {
+					displaySongs(sc);
+				} else if (custName.matches("(?i)R")) {
+					System.out.println("\nRemoving a song!\n");
+					s.removeSong();
+				} else if (custName.matches("(?i)S")) {
+					searchSongs(sc);
+				} else if (custName.matches("(?i)X")) {
+					loop = false;
+					System.out.println("\nReturning to main menu...\n");
+				}
 			}
 		}
-		sc.close();
+
+		else if (choice == 7) {
+			System.out.println("\nGoodbye! ");
+		}
+	}
+	sc.close();
 	}
 	/**
 	 * 
@@ -281,81 +356,28 @@ public class Interface {
 		String state = input.nextLine();
 		String zip_try= input.nextLine();
 		int zip = Integer.parseInt(zip_try);
-		Customer c = new Customer(firstName, lastName, userID, password, email, address, city, state, zip);
-		CustomerDirectory.add(c);
+		Customer c = new Customer(firstName, lastName, email,password,  address, city, state, zip);
+		CustomerDirectory.insert(c);
 		
 		}
 	}
 	
-	/**
-	 * 
-	 */
-	public void currentOrder() throws IOException {
-		System.out.println("\n****CURRENT ORDERS****\n");
-		File orderInfile = new File("currentOrders.txt");
-		Scanner in = new Scanner(orderInfile);
-		while (in.hasNextLine()) {
-			String orderNum = in.nextLine();
-			int num = Integer.parseInt(orderNum);
-			String numItems = in.nextLine();
-			int numItem = Integer.parseInt(numItems);
-			String priority = in.nextLine();
-			int p;
-			if(priority.equalsIgnoreCase("overnight")) {
-				p = 1;
-			}
-			else if(priority.equalsIgnoreCase("rush")) {
-				p = 2;
-			}
-			else{
-				p = 3;
-			}
-			int year = in.nextInt();
-			int month = in.nextInt();
-			int day = in.nextInt();
-			in.nextLine();
-			Calendar cal = Calendar.getInstance();
-			cal.set(year, month, day);
-			String status = in.nextLine();
-			Order o = new Order(num, numItem, p, cal, status);
-			if(status.equalsIgnoreCase("shipped")) {
-				shipped.insert(o);
-			}
-			else {
-				unShipped.insert(o);
-			}
-		}
-//		printShippedOrders();
-//		printUnshippedOrders();
-	}
+//	/**
+//	 * 
+//	 */
+//	public void currentOrder() throws IOException {
+//		System.out.println("\n****CURRENT ORDERS****\n");
+//		File orderInfile = new File("currentOrders.txt");
+//		Scanner in = new Scanner(orderInfile);
+//		while (in.hasNextLine()) {
+//			String orderNum = in.nextLine();
+//			String numItems = in.nextLine();
+//			String status = in.nextLine();
+//			System.out.println(
+//					"Order Number: " + orderNum + "\nNumber of Items: " + numItems + "\nStatuse: " + status + "\n");
+//		}
+//	}
 	
-	public void printShippedOrders() {
-		System.out.println("Shipped Orders: ");
-		shipped.displayArray();
-	}
-	
-	public void printUnshippedOrders() {
-		System.out.println("Unshipped Orders: ");
-		unShipped.displayArray();
-	}
-	
-	
-	/**
-	 * 
-	 */
-	public void Shipment() throws IOException {
-		currentOrder();
-		System.out.println("\n****SHIPMENT DETAILS****\n");
-		System.out.print("These are the ");
-		printUnshippedOrders();
-		Order o = unShipped.getMax();
-		System.out.println("Shipping Order: \n" + o.toString());
-		unShipped.remove(1);
-		shipped.insert(o);
-		System.out.println("Order Number: " + o.getOrderNum() + " has been shipped!\n");
-		System.out.print("Remaining ");
-		printUnshippedOrders();
-	}
 
 ////////////////////////////////////////////////Customer functions//////////////////////////////////////////////////////
 	
@@ -368,15 +390,14 @@ public class Interface {
 		while (in.hasNextLine()) {
 			String firstName = in.nextLine();
 			String lastName = in.nextLine();
-			String userID = in.nextLine();
-			String password = in.nextLine();
 			String email = in.nextLine();
+			String password = in.nextLine();
 			String address = in.nextLine();
 			String city = in.nextLine();
 			String state = in.nextLine();
 			int zip = in.nextInt();
-			Customer c = new Customer(firstName, lastName, userID, password, email, address, city, state, zip);
-			CustomerDirectory.add(c);
+			Customer c = new Customer(firstName, lastName, email,password,  address, city, state, zip);
+			CustomerDirectory.insert(c);
 		}
 	}
 	
@@ -416,12 +437,10 @@ public class Interface {
 		String firstName = input.nextLine();
 		System.out.print("Enter Last name:");
 		String lastName = input.nextLine();
-		System.out.print("Enter user ID:");
-		String userID = input.nextLine();
-		System.out.print("Enter password:");
-		String password = input.nextLine();
 		System.out.print("Enter email:");
 		String email = input.nextLine();
+		System.out.print("Enter password:");
+		String password = input.nextLine();
 		System.out.print("Enter street adress:");
 		String address = input.nextLine();
 		System.out.print("Enter city:");
@@ -430,8 +449,8 @@ public class Interface {
 		String state = input.nextLine();
 		System.out.print("Enter Zip:");
 		int zip = input.nextInt();
-		Customer c = new Customer(firstName, lastName, userID, password, email, address, city, state, zip);
-		CustomerDirectory.add(c);
+		Customer c = new Customer(firstName, lastName, email, password, address, city, state, zip);
+		CustomerDirectory.insert(c);
 		System.out.println("\nWelcome " + firstName + "!\nHow can we help you today?\n");
 		cusMenu();
 	}
@@ -441,19 +460,28 @@ public class Interface {
 	 */
 	public void customerCheck(String email, String password) {
 		Scanner input = new Scanner(System.in);
-		for (int i = 0; i < EmployeeDirectory.size(); i++) {
-			if (CustomerDirectory.get(i).getEmail().equalsIgnoreCase(email)
-					&& CustomerDirectory.get(i).getPassword().equals(password)) {
-				System.out.println("\nWelcome back " + CustomerDirectory.get(i).getFirstName()
-						+ "!\nHow can we help you today?\n");
-				cusMenu();
-			} else {
-				System.out.println("Sorry we could not find an account with the email/password combination!\n");
-				System.out.print("Would you like to create an account now? (Y/N)");
-				String choice1 = input.nextLine();
-				if (choice1.equalsIgnoreCase("Y")) {
+		ArrayList <String> cust= new ArrayList <String> ();
+		File custInfile= new File("customerEmail.txt");
+		while (input.hasNextLine()) {
+			String temp=input.nextLine();
+			cust.add( temp);
+		}
+		for (int i=0; i<cust.size(); i++) {
+			if (cust.get(i).equalsIgnoreCase(email)){
+				for (int j=0; j<cust.size(); j++)
+				{
+					System.out.println("\nHello! Welcome back!\n");
+					cusMenu();
+				}
+			}
+			else {
+				System.out.println("Sorry! We could not find an account with the username.\nWould you like to create a new one?(Y/N)");
+				String newMem= input.nextLine();
+				if (newMem.equalsIgnoreCase("y")){
 					addMember();
-				} else {
+				}
+				else {
+					System.out.println("Logging in as guest...\n");
 					custLogin();
 				}
 			}
@@ -464,21 +492,17 @@ public class Interface {
 	 * 
 	 */
 	public void cusMenu() {
-		Scanner input = new Scanner(System.in);
-		SongsDatabase s = new SongsDatabase();
-		try {
-			s.populateLibary();
-		} catch (IOException e) {
-			System.out.println("IO Error:cusMenu()");
-		}
-		System.out.println("**Main Menu**\n\n" + 
-				  "1. Search/Display songs\n" + 
-				  "2. Place an order\n"
-				+ "3. View recent purchase\n" + 
-				  "4. Exit program\n" + "\n");
-		System.out.print("Please enter your choice (1-4):\n");
-		int choice = input.nextInt();
+		Scanner sc = new Scanner(System.in);
+		int choice = 0;
 		while (choice != 4) {
+			System.out.println("**Main Menu**\n\n" + 
+			           "1. Search/Display songs\n" + 
+			           "2. Place an order\n"
+			         + "3. View recent purchase\n" + 
+			           "4. Exit program\n" + "\n");
+			System.out.print("Please enter your choice (1-4):");
+			String choice_temp = sc.nextLine();
+			choice = Integer.parseInt(choice_temp);
 			if (choice == 1) {
 				boolean loop = true;
 				while (loop) {
@@ -487,22 +511,17 @@ public class Interface {
 							+            "S. Search for a song\n"
 							+            "X. Exit\n");
 					System.out.print("Enter your choice: ");
-					String choice1 = input.next();
-					if (!choice1.equalsIgnoreCase("D") && !choice1.equalsIgnoreCase("S") && choice1.equalsIgnoreCase("X")) {
+					String choice1 = sc.nextLine();
+					if  (!choice1.matches("(?i)D|X|S")){
 						System.out.println("\nInvalid Selection!\n");
 						System.out.print("Enter your choice: ");
-						choice1 = input.next();
+						choice1 = sc.nextLine();
 					} 
 					else if (choice1.matches("(?i)D")) {
-//						try {
-//							displaySongs();
-//						} catch (IOException e) {
-//							e.printStackTrace();
-//						}
-						
+						displaySongs(sc);	
 					} 
 					else if (choice1.matches("(?i)S")) {
-//						searchSongs(sc);
+						searchSongs(sc);
 					} 
 					else if (choice1.matches("(?i)X")) {
 						loop = false;
@@ -515,21 +534,34 @@ public class Interface {
 		 else if (choice == 2) {
 				int numElements = 0;
 				System.out.print("Would you like to Search for the item you want to order? (Y/N)");
-				String temp = input.nextLine();
+				String temp = sc.nextLine();
 				while (!temp.equalsIgnoreCase("N")) {
 					numElements++;
-					s.searchSongPrimary();
-					addToBag(numElements);
+					searchSongs(sc);
+					System.out.print("Would you like to purcahse (Y/N)");
+					String choice1=sc.next();
+					if (choice1.equalsIgnoreCase("y")){
+						System.out.println("Select shipping speed: \n1. Overnight\n2.Rush\n3.Standard");
+						System.out.println("Enter (1-3):");
+						int shippingSpeed= sc.nextInt();
+						placeOrder(numElements,shippingSpeed);
+						orderNum();
+					}
+					
 					System.out.print("Would you like to add more items? (Y/N)");
-					temp = input.nextLine();
+					temp = sc.nextLine();
 				}
 
 			} else if (choice == 3) {
-//////////////////////////////////////////////////////////////
+				Song recent = SongsDatabase.tempSong();
+				System.out.println(SongsDatabase.songsPrimary.getRecentSearch());
+				
 			}
 		}
 		if (choice == 4) {
+			
 			System.out.print("Thank you for shopping with us. Goodbye!");
+			return;
 		}
 
 	}
@@ -537,13 +569,11 @@ public class Interface {
 	/**
 	 * 
 	 */
-	public void addToBag(int numElements) {
-		numElements++;
-		Scanner input = new Scanner(System.in);
-		SongsDatabase s = new SongsDatabase();
-
-		System.out.println();
-		s.searchSongPrimary();
+	public void placeOrder(int numElements, int shippingSpeed) {
+		Calendar cal = Calendar.getInstance();
+		cal.set(2019, 2, 29);
+		String status= "UNSHIPPED";
+		//Order o= new Order(orderNum, numElements, shippingSpeed , cal, status);
 
 	}
 
